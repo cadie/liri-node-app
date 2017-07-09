4235b1702f6db2627dfa474b363b6d7061bde2e8
cadie <cadie@dreamfactoryagency.com>
2017-07-08 20:18:18 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var LastFM = require('last-fm');
+var request = require('request');
+var fs = require("fs");
 
 var getMyTweets = function() {
 
@@ -41,6 +43,38 @@ var searchMusic = function(functionData) {
   }
 }
 
+var movieThis = function(functionData) {
+  request('http://www.omdbapi.com/?apikey=40e9cece&t=' + functionData, function (error, response, body) {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    var jsonData = JSON.parse(body);
+    console.log(
+      'Movie Title: ' + jsonData.Title + '\n' +
+      'Year: ' + jsonData.Year + '\n' +
+      'iMDB Rating: ' + jsonData.Ratings[0].Value + '\n' +
+      'Rotten Tomatoes Rating: ' + jsonData.Ratings[1].Value + '\n' +
+      'Country: ' + jsonData.Country + '\n' +
+      'Language: ' + jsonData.Language + '\n' +
+      'Plot: ' + jsonData.Plot + '\n' +
+      'Actors: ' + jsonData.Actors
+    ); // Print the HTML for the Google homepage.
+  });
+}
+
+var doWhatItSays = function() {
+  fs.readFile('random.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    var dataArr = data.split(',');
+
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1) {
+      pick(dataArr[0]);
+    }
+  });
+}
+
 var pick = function(caseData, functionData) {
   switch (caseData) {
     case 'show-tweets' :
@@ -48,6 +82,12 @@ var pick = function(caseData, functionData) {
       break;
     case 'find-music' :
       searchMusic(functionData);
+      break;
+    case 'movie-this' :
+      movieThis(functionData);
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
       break;
     default:
       console.log('LIRI does not know that');
